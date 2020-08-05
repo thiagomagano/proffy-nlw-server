2f57a7d1545b7ce6c589f2d768cee0e0d9413f67
Thiago Magano <thiagomagano1993@outlook.com>
2020-08-04 21:03:57 -0300

    API finalizada

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,20 @@
 import { Router } from 'express';
 import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 
 const routes = Router();
 
 const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
 
 
 routes.get('/', (req, res) => res.json({ message: "Tudo ok" }))
 
 routes.get('/classes', classesController.index);
 routes.post('/classes', classesController.create);
+
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
 
 export default routes;
